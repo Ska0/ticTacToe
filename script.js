fb12444ea09b8beacaaab5b65a0351a1d395aa98
Jonathan Towery <misfit.for.fame@gmail.com>
2021-12-31 15:11:11 -0600

    working on creating player Objects

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
-const gameBoard = (function() {
-   //create divs, and store them in an array
-   const gameCubeArray = [];
-
-   return {gameCubeArray}
-})()
-
-
-const game = (function() {
-//Game Logic, turn function, startup, playerUI, GameBoard Factory,
-    
-})();
-
 const players = (function() {
-   //Player Factory, Player Objects[name, mark, getNameFunction, getMarkFunction, scoreCardArray]
    const playerMaker = (name, mark) => {
-      let scoreCardArray = ['','','','','','','','','']
       const getName = () => name;
       const getMark = () => mark;
 
-         return {scoreCardArray, getName, getMark}
-   }  
+      return {getName, getMark}
+   }
 
-   const Computer = playerMaker('Computer', 'X')
-
-      return {Computer}
+   return {playerMaker}
 })();
 
+const game = (function() {
+   let name;
+   let player;
+   const startupNameForm = document.forms[0]
+   const startBtn = document.querySelector('#start')
+   const startupModal = document.querySelector('#modal')
+   const startupCloseBtn = document.querySelector('#close')
+   startBtn.addEventListener('click', startup)
+   startupNameForm.addEventListener('submit', (e) =>{
+      e.preventDefault();
 
+   })
+   startupCloseBtn.addEventListener('click', closeStartup)
+   function startup() {
+      startupModal.style.display = 'flex'
 
+   }
 
+   function closeStartup() {
+      startupModal.style.display = 'none'
+      name = startupNameForm[0].value
+      console.log(name)
+       player = players.playerMaker(name, 'O');
+   }
+
+   function createGameBoard() {
+      const gameArea = document.querySelector('.game-container')
+      for (i = 0; i < 9; i++) {
+         let gameCube = document.createElement('div');
+         gameCube.classList.add('game-cube');
+         gameCube.dataset.cube = i;
+         gameArea.appendChild(gameCube)
+
+      }
+   }
+
+   return {player}
+
+})();
 
